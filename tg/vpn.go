ae363d1ed6869c3d5490eb591925af0294ab998e
terry heath <theath@gmail.com>
2022-11-16 12:33:23 +1300

    send up an empty list

--- tg/vpn.go
+++ tg/vpn.go
@@ -11,7 +11,7 @@ type VPNInterface struct {
 	InterfaceName   string            `json:"interfaceName,omitempty"`
 	InDefaultRoute  bool              `json:"inDefaultRoute"`
 	OutDefaultRoute bool              `json:"outDefaultRoute"`
-	InsideNATs      []VPNInterfaceNAT `json:"insideNats,omitempty"`
-	OutsideNATs     []VPNInterfaceNAT `json:"outsideNats,omitempty"`
+	InsideNATs      []VPNInterfaceNAT `json:"insideNats"`
+	OutsideNATs     []VPNInterfaceNAT `json:"outsideNats"`
 	Description     string            `json:"description,omitempty"`
 }
