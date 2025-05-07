55f4766ca5480bb1d0bd19f08e87bddaf3cbd888
terry heath <theath@gmail.com>
2025-05-07 10:08:34 -0600

    add some more acceptance tests bc they are awesome (#140)

---
+++ acctests/clusterconfig_test.go
@@ -0,0 +1,132 @@
+package acctests
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-testing/compare"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+	"github.com/trustgrid/terraform-provider-tg/provider"
+	"github.com/trustgrid/terraform-provider-tg/tg"
+)
+
+func TestAccClusterConfig_HappyPath(t *testing.T) {
+	compareValuesSame := statecheck.CompareValue(compare.ValuesSame())
+	nodeID := "d70e7d73-2a1c-4388-bbb1-08ca2fd39f48"
+
+	provider := provider.New("test")()
+
+	resource.Test(t, resource.TestCase{
+		Providers: map[string]*schema.Provider{
+			"tg": provider,
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: clusterConfigTestConfig(nodeID, true, "10.20.30.40", 7946, "10.20.30.41", 7947, true),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("tg_node_cluster_config.test", "id", nodeID),
+					resource.TestCheckResourceAttr("tg_node_cluster_config.test", "node_id", nodeID),
+					resource.TestCheckResourceAttr("tg_node_cluster_config.test", "enabled", "true"),
+					resource.TestCheckResourceAttr("tg_node_cluster_config.test", "host", "10.20.30.40"),
+					resource.TestCheckResourceAttr("tg_node_cluster_config.test", "port", "7946"),
+					resource.TestCheckResourceAttr("tg_node_cluster_config.test", "status_host", "10.20.30.41"),
+					resource.TestCheckResourceAttr("tg_node_cluster_config.test", "status_port", "7947"),
+					resource.TestCheckResourceAttr("tg_node_cluster_config.test", "active", "true"),
+					checkClusterConfigAPISide(provider, nodeID, true, "10.20.30.40", 7946, "10.20.30.41", 7947),
+				),
+				ConfigStateChecks: []statecheck.StateCheck{
+					compareValuesSame.AddStateValue("tg_node_cluster_config.test", tfjsonpath.New("id")),
+				},
+			},
+			{
+				Config: clusterConfigTestConfig(nodeID, true, "10.20.30.50", 7950, "10.20.30.51", 7951, true),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("tg_node_cluster_config.test", "id", nodeID),
+					resource.TestCheckResourceAttr("tg_node_cluster_config.test", "node_id", nodeID),
+					resource.TestCheckResourceAttr("tg_node_cluster_config.test", "enabled", "true"),
+					resource.TestCheckResourceAttr("tg_node_cluster_config.test", "host", "10.20.30.50"),
+					resource.TestCheckResourceAttr("tg_node_cluster_config.test", "port", "7950"),
+					resource.TestCheckResourceAttr("tg_node_cluster_config.test", "status_host", "10.20.30.51"),
+					resource.TestCheckResourceAttr("tg_node_cluster_config.test", "status_port", "7951"),
+					resource.TestCheckResourceAttr("tg_node_cluster_config.test", "active", "true"),
+					checkClusterConfigAPISide(provider, nodeID, true, "10.20.30.50", 7950, "10.20.30.51", 7951),
+				),
+				ConfigStateChecks: []statecheck.StateCheck{
+					compareValuesSame.AddStateValue("tg_node_cluster_config.test", tfjsonpath.New("id")),
+				},
+			},
+			{
+				Config: clusterConfigTestConfig(nodeID, false, "10.20.30.50", 7950, "10.20.30.51", 7951, true),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("tg_node_cluster_config.test", "id", nodeID),
+					resource.TestCheckResourceAttr("tg_node_cluster_config.test", "node_id", nodeID),
+					resource.TestCheckResourceAttr("tg_node_cluster_config.test", "enabled", "false"),
+					resource.TestCheckResourceAttr("tg_node_cluster_config.test", "host", "10.20.30.50"),
+					resource.TestCheckResourceAttr("tg_node_cluster_config.test", "port", "7950"),
+					resource.TestCheckResourceAttr("tg_node_cluster_config.test", "status_host", "10.20.30.51"),
+					resource.TestCheckResourceAttr("tg_node_cluster_config.test", "status_port", "7951"),
+					resource.TestCheckResourceAttr("tg_node_cluster_config.test", "active", "true"),
+					checkClusterConfigAPISide(provider, nodeID, false, "10.20.30.50", 7950, "10.20.30.51", 7951),
+				),
+				ConfigStateChecks: []statecheck.StateCheck{
+					compareValuesSame.AddStateValue("tg_node_cluster_config.test", tfjsonpath.New("id")),
+				},
+			},
+		},
+	})
+}
+
+func clusterConfigTestConfig(nodeID string, enabled bool, host string, port int, statusHost string, statusPort int, active bool) string {
+	return fmt.Sprintf(`
+resource "tg_node_cluster_config" "test" {
+  node_id      = "%s"
+  enabled      = %t
+  host         = "%s"
+  port         = %d
+  status_host  = "%s"
+  status_port  = %d
+  active       = %t
+}
+	`, nodeID, enabled, host, port, statusHost, statusPort, active)
+}
+
+func checkClusterConfigAPISide(provider *schema.Provider, nodeID string, enabled bool, host string, port int, statusHost string, statusPort int) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		client := provider.Meta().(*tg.Client)
+
+		var node tg.Node
+		err := client.Get(context.Background(), fmt.Sprintf("/node/%s", nodeID), &node)
+		if err != nil {
+			return fmt.Errorf("error getting node: %w", err)
+		}
+
+		clusterConfig := node.Config.Cluster
+
+		if clusterConfig.Enabled != enabled {
+			return fmt.Errorf("expected cluster config enabled to be %t, got %t", enabled, clusterConfig.Enabled)
+		}
+
+		if clusterConfig.Host != host {
+			return fmt.Errorf("expected cluster config host to be %s, got %s", host, clusterConfig.Host)
+		}
+
+		if clusterConfig.Port != port {
+			return fmt.Errorf("expected cluster config port to be %d, got %d", port, clusterConfig.Port)
+		}
+
+		if clusterConfig.StatusHost != statusHost {
+			return fmt.Errorf("expected cluster config status_host to be %s, got %s", statusHost, clusterConfig.StatusHost)
+		}
+
+		if clusterConfig.StatusPort != statusPort {
+			return fmt.Errorf("expected cluster config status_port to be %d, got %d", statusPort, clusterConfig.StatusPort)
+		}
+
+		return nil
+	}
+}
---
+++ acctests/idp_test.go
@@ -0,0 +1,150 @@
+package acctests
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-testing/compare"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+	"github.com/trustgrid/terraform-provider-tg/provider"
+	"github.com/trustgrid/terraform-provider-tg/tg"
+)
+
+func TestAccIDP_HappyPath(t *testing.T) {
+	compareValuesSame := statecheck.CompareValue(compare.ValuesSame())
+	idpName := "tf-test-idp"
+	idpDescription := "Terraform test IDP"
+	updatedDescription := "Updated Terraform test IDP"
+
+	provider := provider.New("test")()
+
+	resource.Test(t, resource.TestCase{
+		Providers: map[string]*schema.Provider{
+			"tg": provider,
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: idpConfig(idpName, "SAML", idpDescription),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("tg_idp.test", "id"),
+					resource.TestCheckResourceAttrSet("tg_idp.test", "uid"),
+					resource.TestCheckResourceAttr("tg_idp.test", "name", idpName),
+					resource.TestCheckResourceAttr("tg_idp.test", "type", "SAML"),
+					resource.TestCheckResourceAttr("tg_idp.test", "description", idpDescription),
+					checkIDPAPISide(provider, idpName, "SAML", idpDescription),
+				),
+				ConfigStateChecks: []statecheck.StateCheck{
+					compareValuesSame.AddStateValue("tg_idp.test", tfjsonpath.New("id")),
+				},
+			},
+			{
+				Config: idpConfig(idpName, "SAML", updatedDescription),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("tg_idp.test", "id"),
+					resource.TestCheckResourceAttrSet("tg_idp.test", "uid"),
+					resource.TestCheckResourceAttr("tg_idp.test", "name", idpName),
+					resource.TestCheckResourceAttr("tg_idp.test", "type", "SAML"),
+					resource.TestCheckResourceAttr("tg_idp.test", "description", updatedDescription),
+					checkIDPAPISide(provider, idpName, "SAML", updatedDescription),
+				),
+				ConfigStateChecks: []statecheck.StateCheck{
+					compareValuesSame.AddStateValue("tg_idp.test", tfjsonpath.New("id")),
+				},
+			},
+		},
+	})
+}
+
+func TestAccIDP_MultipleTypes(t *testing.T) {
+	idpBaseName := "tf-test-idp"
+	idpDescription := "Terraform test IDP"
+
+	provider := provider.New("test")()
+
+	resource.Test(t, resource.TestCase{
+		Providers: map[string]*schema.Provider{
+			"tg": provider,
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: idpConfig(idpBaseName+"-openid", "OpenID", idpDescription),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("tg_idp.test", "id"),
+					resource.TestCheckResourceAttr("tg_idp.test", "type", "OpenID"),
+					checkIDPAPISide(provider, idpBaseName+"-openid", "OpenID", idpDescription),
+				),
+			},
+			{
+				Config: idpConfig(idpBaseName+"-azure", "AzureAD", idpDescription),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("tg_idp.test", "id"),
+					resource.TestCheckResourceAttr("tg_idp.test", "type", "AzureAD"),
+					checkIDPAPISide(provider, idpBaseName+"-azure", "AzureAD", idpDescription),
+				),
+			},
+			{
+				Config: idpConfig(idpBaseName+"-gsuite", "GSuite", idpDescription),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("tg_idp.test", "id"),
+					resource.TestCheckResourceAttr("tg_idp.test", "type", "GSuite"),
+					checkIDPAPISide(provider, idpBaseName+"-gsuite", "GSuite", idpDescription),
+				),
+			},
+		},
+	})
+}
+
+func idpConfig(name string, idpType string, description string) string {
+	return fmt.Sprintf(`
+resource "tg_idp" "test" {
+  name        = "%s"
+  type        = "%s"
+  description = "%s"
+}
+	`, name, idpType, description)
+}
+
+func checkIDPAPISide(provider *schema.Provider, name string, idpType string, description string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		client := provider.Meta().(*tg.Client)
+
+		rs, ok := s.RootModule().Resources["tg_idp.test"]
+		if !ok {
+			return fmt.Errorf("IDP resource not found")
+		}
+
+		idpUID := rs.Primary.ID
+		if idpUID == "" {
+			return fmt.Errorf("no IDP ID is set")
+		}
+
+		var idp tg.IDP
+		err := client.Get(context.Background(), "/v2/idp/"+idpUID, &idp)
+		if err != nil {
+			return fmt.Errorf("error getting IDP: %w", err)
+		}
+
+		if idp.Name != name {
+			return fmt.Errorf("expected IDP name to be %s, got %s", name, idp.Name)
+		}
+
+		if idp.Type != idpType {
+			return fmt.Errorf("expected IDP type to be %s, got %s", idpType, idp.Type)
+		}
+
+		if idp.Description != description {
+			return fmt.Errorf("expected IDP description to be %s, got %s", description, idp.Description)
+		}
+
+		if idp.UID != idpUID {
+			return fmt.Errorf("expected IDP UID to be %s, got %s", idpUID, idp.UID)
+		}
+
+		return nil
+	}
+}
