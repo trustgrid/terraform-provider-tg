5e2b1e97db5109529270357bcf649dfddd46fde0
terry heath <theath@gmail.com>
2026-04-09 12:57:30 -0600

    Use unique names in vnet acceptance tests

--- acctests/vnetgroupmembership_test.go
+++ acctests/vnetgroupmembership_test.go
@@ -7,6 +7,7 @@ import (
 
 	_ "embed"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-testing/compare"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -17,13 +18,13 @@ import (
 	"github.com/trustgrid/terraform-provider-tg/tg"
 )
 
-//go:embed test-data/vnet-group-membership/create.hcl
-var vnetMembership string
-
 func TestAccVirtualNetworkGroupMembership_HappyPath(t *testing.T) {
 	compareValuesSame := statecheck.CompareValue(compare.ValuesSame())
 
 	provider := provider.New("test")()
+	networkName := "tf-test-membership-" + acctest.RandStringFromCharSet(8, acctest.CharSetAlphaNum)
+	groupName := "tf-test-group-" + acctest.RandStringFromCharSet(8, acctest.CharSetAlphaNum)
+	objectName := "tf-test-obj-" + acctest.RandStringFromCharSet(8, acctest.CharSetAlphaNum)
 
 	resource.Test(t, resource.TestCase{
 		Providers: map[string]*schema.Provider{
@@ -31,13 +32,13 @@ func TestAccVirtualNetworkGroupMembership_HappyPath(t *testing.T) {
 		},
 		Steps: []resource.TestStep{
 			{
-				Config: vnetMembership,
+				Config: vnetMembershipConfig(networkName, groupName, objectName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet("tg_virtual_network_group_membership.test", "id"),
-					resource.TestCheckResourceAttr("tg_virtual_network_group_membership.test", "object", "test-obj"),
-					resource.TestCheckResourceAttr("tg_virtual_network_group_membership.test", "group", "test-group"),
-					resource.TestCheckResourceAttr("tg_virtual_network_group_membership.test", "network", "test-membership"),
-					checkVNetMembershipAPI(provider),
+					resource.TestCheckResourceAttr("tg_virtual_network_group_membership.test", "object", objectName),
+					resource.TestCheckResourceAttr("tg_virtual_network_group_membership.test", "group", groupName),
+					resource.TestCheckResourceAttr("tg_virtual_network_group_membership.test", "network", networkName),
+					checkVNetMembershipAPI(provider, networkName, groupName, objectName),
 				),
 				ConfigStateChecks: []statecheck.StateCheck{
 					compareValuesSame.AddStateValue("tg_virtual_network_group_membership.test", tfjsonpath.New("id")),
@@ -47,17 +48,42 @@ func TestAccVirtualNetworkGroupMembership_HappyPath(t *testing.T) {
 	})
 }
 
-func checkVNetMembershipAPI(provider *schema.Provider) resource.TestCheckFunc {
+func vnetMembershipConfig(networkName string, groupName string, objectName string) string {
+	return fmt.Sprintf(`
+resource "tg_virtual_network" "member_test" {
+  name = %q
+}
+
+resource "tg_virtual_network_group" "test" {
+  name    = %q
+  network = tg_virtual_network.member_test.name
+}
+
+resource "tg_virtual_network_object" "test" {
+  name    = %q
+  cidr    = "10.10.20.0/24"
+  network = tg_virtual_network.member_test.name
+}
+
+resource "tg_virtual_network_group_membership" "test" {
+  object  = tg_virtual_network_object.test.name
+  group   = tg_virtual_network_group.test.name
+  network = tg_virtual_network.member_test.name
+}
+`, networkName, groupName, objectName)
+}
+
+func checkVNetMembershipAPI(provider *schema.Provider, networkName string, groupName string, objectName string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		client := provider.Meta().(*tg.Client)
 
 		var membership []tg.VNetGroupMembership
-		if err := client.Get(context.Background(), "/v2/domain/"+client.Domain+"/network/test-membership/network-group/test-group", &membership); err != nil {
+		if err := client.Get(context.Background(), "/v2/domain/"+client.Domain+"/network/"+networkName+"/network-group/"+groupName, &membership); err != nil {
 			return fmt.Errorf("error getting vnet group membership: %w", err)
 		}
 
 		for _, obj := range membership {
-			if obj.Object == "test-obj" && obj.Group == "test-group" {
+			if obj.Object == objectName && obj.Group == groupName {
 				return nil
 			}
 		}
--- acctests/vnetobject_test.go
+++ acctests/vnetobject_test.go
@@ -8,6 +8,7 @@ import (
 
 	_ "embed"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-testing/compare"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -37,16 +38,12 @@ func init() {
 	})
 }
 
-//go:embed test-data/vnet-object/create.hcl
-var vnetObjCreate string
-
-//go:embed test-data/vnet-object/update.hcl
-var vnetObjUpdate string
-
 func TestAccVirtualNetworkObject_HappyPath(t *testing.T) {
 	compareValuesSame := statecheck.CompareValue(compare.ValuesSame())
 
 	provider := provider.New("test")()
+	networkName := "tf-test-vnet-obj-" + acctest.RandStringFromCharSet(8, acctest.CharSetAlphaNum)
+	objectName := "tf-test-obj-" + acctest.RandStringFromCharSet(8, acctest.CharSetAlphaNum)
 
 	resource.Test(t, resource.TestCase{
 		Providers: map[string]*schema.Provider{
@@ -54,24 +51,24 @@ func TestAccVirtualNetworkObject_HappyPath(t *testing.T) {
 		},
 		Steps: []resource.TestStep{
 			{
-				Config: vnetObjCreate,
+				Config: vnetObjectConfig(networkName, objectName, "10.10.20.0/24"),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet("tg_virtual_network_object.test", "id"),
-					resource.TestCheckResourceAttr("tg_virtual_network_object.test", "name", "test-obj"),
+					resource.TestCheckResourceAttr("tg_virtual_network_object.test", "name", objectName),
 					resource.TestCheckResourceAttr("tg_virtual_network_object.test", "cidr", "10.10.20.0/24"),
-					checkVNetObjAPI(provider, "10.10.20.0/24"),
+					checkVNetObjAPI(provider, networkName, objectName, "10.10.20.0/24"),
 				),
 				ConfigStateChecks: []statecheck.StateCheck{
 					compareValuesSame.AddStateValue("tg_virtual_network_object.test", tfjsonpath.New("id")),
 				},
 			},
 			{
-				Config: vnetObjUpdate,
+				Config: vnetObjectConfig(networkName, objectName, "10.10.30.0/24"),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet("tg_virtual_network_object.test", "id"),
-					resource.TestCheckResourceAttr("tg_virtual_network_object.test", "name", "test-obj"),
+					resource.TestCheckResourceAttr("tg_virtual_network_object.test", "name", objectName),
 					resource.TestCheckResourceAttr("tg_virtual_network_object.test", "cidr", "10.10.30.0/24"),
-					checkVNetObjAPI(provider, "10.10.30.0/24"),
+					checkVNetObjAPI(provider, networkName, objectName, "10.10.30.0/24"),
 				),
 				ConfigStateChecks: []statecheck.StateCheck{
 					compareValuesSame.AddStateValue("tg_virtual_network_object.test", tfjsonpath.New("id")),
@@ -81,12 +78,29 @@ func TestAccVirtualNetworkObject_HappyPath(t *testing.T) {
 	})
 }
 
-func checkVNetObjAPI(provider *schema.Provider, cidr string) resource.TestCheckFunc {
+func vnetObjectConfig(networkName string, objectName string, cidr string) string {
+	return fmt.Sprintf(`
+resource "tg_virtual_network" "obj_network" {
+  name         = %q
+  network_cidr = "10.10.0.0/16"
+  description  = "Object Test Virtual Network"
+  no_nat       = false
+}
+
+resource "tg_virtual_network_object" "test" {
+  name    = %q
+  cidr    = %q
+  network = tg_virtual_network.obj_network.name
+}
+`, networkName, objectName, cidr)
+}
+
+func checkVNetObjAPI(provider *schema.Provider, networkName string, objectName string, cidr string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		client := provider.Meta().(*tg.Client)
 
 		var obj tg.VNetObject
-		if err := client.Get(context.Background(), "/v2/domain/"+client.Domain+"/network/test-obj/network-object/test-obj", &obj); err != nil {
+		if err := client.Get(context.Background(), "/v2/domain/"+client.Domain+"/network/"+networkName+"/network-object/"+objectName, &obj); err != nil {
 			return fmt.Errorf("error getting vnet object: %w", err)
 		}
 
